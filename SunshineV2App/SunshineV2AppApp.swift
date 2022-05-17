4b94050ed1d0879f6f8afad18b4191ba86ac4c55
marcus2188 <marcus.2188@gmail.com>
2022-05-17 12:40:11 +0800

    Initial Commit

---
+++ SunshineV2App/SunshineV2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunshineV2AppApp.swift
+//  SunshineV2App
+//
+//  Created by Ksk I on 17/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SunshineV2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
